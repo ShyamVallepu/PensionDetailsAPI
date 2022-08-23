0264cbb1402500f2cee8cf5e326b515dac44b303
Shyam Sekhar Vadde <shyamvallepu8@gmail.com>
2022-08-23 16:42:50 +0530

    Removed CSV file dependency

--- Pensioner_Details/Repository/PensionerRepository.cs
+++ Pensioner_Details/Repository/PensionerRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -27,31 +28,56 @@ namespace Pensioner_Details.Repository
         {
             _log4net.Info("Data is read from CSV file");  // Logging Implemented
             List<PensionerDetail> pensionerdetail = new List<PensionerDetail>();
-            string line;
-            try
+            //string line;
+            //try
+            //{
+
+            //    using (StreamReader sr = new StreamReader(@".\details.csv"))
+            //    {
+            //        //string line;
+            //        while ((line = sr.ReadLine()) != null)
+            //        {
+            //            string[] values = line.Split(',');
+            //            pensionerdetail.Add(new PensionerDetail() { Name = values[0], Dateofbirth = Convert.ToDateTime(values[1]), Pan = values[2], AadharNumber = values[3], SalaryEarned = Convert.ToInt32(values[4]), Allowances = Convert.ToInt32(values[5]), PensionType = (PensionTypeValue)Enum.Parse(typeof(PensionTypeValue), values[6]), BankName = values[7], AccountNumber = values[8], BankType = (BankType)Enum.Parse(typeof(BankType), values[9]) });
+            //        }
+
+            //    }
+            //}
+
+
+
+            //catch (NullReferenceException e)
+            //{
+            //    _log4net.Error("Values cannot be fetched from the Csv file" + e);
+            //    return null;
+            //}
+            //catch (Exception e)
+            //{
+            //    _log4net.Error("Values cannot be fetched from the Csv file" + e);
+            //    return null;
+            //}
+            var ConnectionString = "DefaultEndpointsProtocol=https;AccountName=pensiondetailscsv;AccountKey=hUwxzhgJxAmHlISaKlmuZ06F3WCFqrWxSbDLnvGf4pJWssY+uKvKA4W18i/2mcOkkA8vyBl/27xG+ASt5W/hXg==;EndpointSuffix=core.windows.net";
+
+            BlobServiceClient blobServiceClient = new BlobServiceClient(ConnectionString);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient("publiccsv");
+            BlobClient blobClient = containerClient.GetBlobClient("details.csv");
+            if (blobClient.Exists())
             {
-                
-                using (StreamReader sr = new StreamReader(@".\details.csv"))
+                var response = blobClient.Download();
+                using (var streamReader = new StreamReader(response.Value.Content))
                 {
-                    //string line;
-                    while ((line = sr.ReadLine()) != null)
+                    while (!streamReader.EndOfStream)
                     {
+                        var line = streamReader.ReadLine();
                         string[] values = line.Split(',');
                         pensionerdetail.Add(new PensionerDetail() { Name = values[0], Dateofbirth = Convert.ToDateTime(values[1]), Pan = values[2], AadharNumber = values[3], SalaryEarned = Convert.ToInt32(values[4]), Allowances = Convert.ToInt32(values[5]), PensionType = (PensionTypeValue)Enum.Parse(typeof(PensionTypeValue), values[6]), BankName = values[7], AccountNumber = values[8], BankType = (BankType)Enum.Parse(typeof(BankType), values[9]) });
                     }
-
                 }
             }
-            catch (NullReferenceException e)
-            {
-                _log4net.Error("Values cannot be fetched from the Csv file" + e);
-                return null;
-            }
-            catch (Exception e)
-            {
-                _log4net.Error("Values cannot be fetched from the Csv file" + e);
-                return null;
-            }
+
+
+
+
             return pensionerdetail.ToList();
         }
 
